e7d552547f6c85c25eb3b31a37feded160f057ec
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-10-03 18:22:41 +0530

    Interface Example

---
+++ oops/InterfaceDemo.java
@@ -0,0 +1,39 @@
+package oops;
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        S s = new S(200, 10);
+        System.out.println(s.getMaxSpeed());
+
+        System.out.println(s.getStdMileage());
+    }
+}
+
+interface Car {
+    static String companyName = "Mercedes";
+
+    public abstract float getStdMileage();
+
+    public abstract int getMaxSpeed();
+}
+
+class S implements Car {
+    float mileage;
+    int maxSpeed;
+
+    S(int maxSpeed, float mileage) {
+        this.maxSpeed = maxSpeed;
+        this.mileage = mileage;
+    }
+
+    @Override
+    public float getStdMileage() {
+        return this.mileage;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+}
